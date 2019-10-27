bf660313a16cd90f0873114dcc0ea00889eb3f30
BrazhnikovV <brazhnikov_ve@mail.ru>
2019-10-27 14:33:00 +0300

    Поключил страницы аутентификации и главную.

--- src/main/java/ru/brazhnikov/todolist/controllers/MyErrorController.java
+++ src/main/java/ru/brazhnikov/todolist/controllers/MyErrorController.java
@@ -1,8 +1,16 @@
 package ru.brazhnikov.todolist.controllers;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 /**
  * MyErrorController - класс контроллер для обработки 404-й ошибки
@@ -16,8 +24,12 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 @Controller
 class MyErrorController implements ErrorController {
 
-    @RequestMapping("/error")
+    @RequestMapping( value = "/error")
     public String handleError() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if ((authentication instanceof AnonymousAuthenticationToken)) {
+            return "redirect:/login";
+        }
         return "/pages/errors/error404";
     }
 
